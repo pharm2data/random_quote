790a2bd48b32593b36be7d3ce50f219c2f54ac90
pharm2data <mollystephens321@gmail.com>
2024-03-03 20:31:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,17 @@ const usedIndexes = new Set()
 const quoteElement = document.getElementById("quote")
 
 function generateQuote() {
-    const randomIdx = Math.floor(Math.random() * quote.length)
+    if(usedIndexes.size >= quotes.length) {
+    usedIndexes.clear()
+}
+    while (true) {
+        const randomIdx = Math.floor(Math.random() * quote.length)
+        if (usedIndexes.has(randomIdx)) continue
+    
+    
     const quote = quotes[randomIdx]
     quoteElement.innerHTML = quote;
+    usedIndexes.add(randomIdx)
+    break
+    }
 }
